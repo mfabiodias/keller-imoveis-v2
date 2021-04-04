d5c92c7a0a4c9b0effc0b6354ea863fea4cbaa32
Fabio Dias <mfabiodias@gmail.com>
2021-04-04 12:50:51 -0300

    🐛 Logout action error

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import { App } from '@inertiajs/inertia-react';
 import React from 'react';
 import { render } from 'react-dom';
 import 'antd/dist/antd.css';
+require('./bootstrap');
+require('alpinejs');
 
 const el = document.getElementById('app');
 
